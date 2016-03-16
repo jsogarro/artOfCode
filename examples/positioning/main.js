c20ed499197fdb6fbe5cd58ba493f7339bb6b338
Jamal O'Garro <jamal.ogarro@gmail.com>
2016-03-15 22:51:35 -0400

    added jQuery form input examples

---
+++ examples/positioning/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Get text from first name input field
+var inputFirstNameValue = $('input#first').val();
+
+// Add event listener to button
+$('button').on('click', function() {
+    // TODO: Write some logic to get the values out of the inputs and do sometthing with it
+    console.log('clicked!!!');
+});
